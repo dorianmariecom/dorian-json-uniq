2353d478e4be61176e98e860deec7e8f3acdc9fc
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 00:44:04 +0200

    update dorian-arguments and refactor test

--- spec/json_uniq_spec.rb
+++ spec/json_uniq_spec.rb
@@ -4,8 +4,6 @@ require "spec_helper"
 
 RSpec.describe "json-uniq" do
   it "works" do
-    expect(
-      `bin/json-uniq "[1, 1, 2]" --pretty false`
-    ).to eq("[1,2]\n")
+    expect(`bin/json-uniq "[1, 1, 2]" --pretty false`).to eq("[1,2]\n")
   end
 end
